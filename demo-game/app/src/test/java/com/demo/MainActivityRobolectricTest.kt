eae11d41bdd3db760e3f0c98a681ba7f21f8ccda
up1 <somkiat.p@gmail.com>
2020-09-22 08:38:19 +0700

    Using rebolectric with androidx

---
+++ demo-game/app/src/test/java/com/demo/MainActivityRobolectricTest.kt
@@ -0,0 +1,16 @@
+package com.demo
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityRobolectricTest {
+    @Test
+    fun start_new_game() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.recreate()
+    }
+}
